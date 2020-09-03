68c19af4d6b8204caeee1271dc5c38cccb1dc8d1
Abril-P <70672628+Abril-P@users.noreply.github.com>
2020-09-02 18:45:29 -0700

    Delete Analytics AP.py

--- Analytics AP.py
+++
@@ -1,7 +0,0 @@
-Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-'''
-What is Analytics?
-Analytics is using statistics and computing to analyze raw data and transform it into information that can be of value. This information can be used to make decisions based on the patterns, relationships, and predictions of the outcome.
-'''
